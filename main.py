e1a445de719dcacc6a9af3e6aaf29ea4871c36d7
Ivan <Salik52@yandex.ru>
2020-10-08 11:52:48 +0300

    Добавлены поые комментарии в кодяснитенльн

--- main.py
+++ main.py
@@ -1,34 +1,34 @@
-def in_ASCII(string):
+def in_ASCII(string): #функия представления символов в числовой код таблицы ASCII
     a = []
     for i in string:
         a.append(ord(i))
     return a
 
 
-def encryption(list, step):
+def encryption(list, step): #функция дообавление шага к нужным символам
     new_list = []
     for i in list:
         if i in AZ:
             i += step
-            if i > 90:
+            if i > 90: #проверка
                 i = (i % 90) + 65
         elif i in az:
             i += step
-            if i > 122:
+            if i > 122: #проверка
                 i = (i % 122) + 97
         elif i in AZ_Rus:
             i += step
-            if i > 1071:
+            if i > 1071: #проверка
                 i = (i % 1071) + 1040
         elif i in az_Rus:
             i += step
-            if i > 1103:
+            if i > 1103: #проверка
                 i = (i % 1103) + 1072
         new_list.append(i)
     return new_list
 
 
-def decryption(new_list):
+def decryption(new_list): #функция перевода числовых значений таблицы ASCII в символы
     a = str()
     for i in new_list:
         a += chr(i)
@@ -42,7 +42,6 @@ az_Rus = set(i for i in range(1072, 1104))
 
 decr = open('source.txt', 'r')
 encr = open('encryption.txt', 'w')
-# s = input()
 step = int(input())
 for line in decr:
     encr.write(decryption(encryption(in_ASCII(line), step)))
